6c637523fca9b4a5c2e306b72b9ab44bd331bf2d
Galih Muhammad Ichsan <79683446+Frezneel@users.noreply.github.com>
2021-10-31 18:27:24 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
+$host = "remotemysql.com";
+$user = "NOtGhlwFPt";
+$pass = "ZsDoNNcXPU";
 $db = "spd_db";
 
 $konek = mysqli_connect($host, $user, $pass, $db) or die("Database MYSQL Tidak Terhubung"); 
